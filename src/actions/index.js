cff7f590748232ed1a0dcae691a74ad6c9de554f
Tara Sherman <taradelin@gmail.com>
2019-07-16 21:07:33 -0400

    action creators  set up

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,27 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+import axios from 'axios';
+
+// loading state
+export const GET_CHARACTERS_START = 'GET_CHARACTERS_START'
+// if successfully completed
+export const GET_CHARACTERS_SUCCESS = 'GET_CHARACTERS_SUCCESS'
+// if failed
+export const GET_CHARACTERS_FAILED = 'GET_CHARACTERS_FAILED'
+
+// action creator
+export function getCharacters() {
+    return (dispatch) => {
+        dispatch({ type: GET_CHARACTERS_START})
+
+        axios
+            .get('https://swapi.co/api/people/')
+            .then((response) => {
+                dispatch({ type: GET_CHARACTERS_SUCCESS, payload: response.data})
+            })
+            .catch((error) => {
+                dispatch({ type: GET_CHARACTERS_FAILED, payload: error.response.data})
+            })
+    }
+}
