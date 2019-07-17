a8a95a97850173036326a61e5bd73c32b18cd0bc
Tara Sherman <taradelin@gmail.com>
2019-07-16 20:17:56 -0400

    remembered to use 'compose'

--- src/index.js
+++ src/index.js
@@ -15,9 +15,11 @@ import logger from 'redux-logger';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk, logger),
+  compose(
+    applyMiddleware(thunk, logger),
   /* applyMiddleware goes here */
   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
 );
 
 ReactDOM.render(
